f2a08dc400a637a994d441849d96aa373625726f
gaoruixian <gaoruixian@chuxing365.com>
2023-05-16 21:16:09 +0800

    1335 dynamicprogramming

--- leetcode/1335minDifficulty_test.go
+++ leetcode/1335minDifficulty_test.go
@@ -50,47 +50,38 @@ func minDifficulty3(jobDifficulty []int, d int) int {
 
 }
 
-func minDifficulty(job []int, d int) int {
-	jobLen := len(job)
-
-	if len(job) < d {
+func minDifficulty(jobDifficulty []int, d int) int {
+	jobLen := len(jobDifficulty)
+	if len(jobDifficulty) < d {
 		return -1
 	}
-
 	dp := make([][]int, jobLen+1) // dp[nJob][nDay]表示前nJob个任务分配到nDay天的困难度
 	dp[0] = make([]int, d+1)
 	for i := 0; i < d+1; i++ {
-		// [[1073741823 1073741823 1073741823 1073741823 1073741823 1073741823 1073741823] [] [] [] [] [] [] [] []]
-		dp[0][i] = math.MaxInt32 / 2
+		dp[0][i] = 300001
 	}
-	fmt.Println(dp)
-	dp[0][0] = 0 //关键
+	dp[0][0] = 0
 
 	for nJob := 1; nJob <= jobLen; nJob++ {
 		dp[nJob] = make([]int, d+1)
-		dp[nJob][0] = math.MaxInt32 / 2
+		dp[nJob][0] = 300001
 
 		//因为每天至少要有一个任务，所以nJob个任务不可能分配到nJob+1以上的天中
 		for nDay := 1; nDay <= nJob && nDay < d+1; nDay++ {
-
 			md := 0
-			dp[nJob][nDay] = math.MaxInt32 / 2
-
+			dp[nJob][nDay] = 300001
 			//这里 从后往前，就可以快速获取中当前天的最大值
 			for j := nJob - 1; j >= nDay-1; j-- {
-				if job[j] > md {
-					md = job[j]
+				if jobDifficulty[j] > md {
+					md = jobDifficulty[j]
 				}
 				//比较 前j个任务分配到nDay-1天的困难程度 加上 剩余任务（分配到了nday天）的最大困难程度
 				if dp[j][nDay-1]+md < dp[nJob][nDay] {
 					dp[nJob][nDay] = dp[j][nDay-1] + md
 				}
 			}
-
 		}
-
 	}
-	// [[0 1073741823 1073741823 1073741823 1073741823 1073741823 1073741823] [1073741823 11 0 0 0 0 0] [1073741823 111 122 0 0 0 0] [1073741823 111 122 144 0 0 0] [1073741823 222 233 344 366 0 0] [1073741823 222 233 266 366 399 0] [1073741823 333 344 455 477 699 732] [1073741823 333 344 388 477 521 732] [1073741823 444 455 566 588 810 843]]
 	return dp[jobLen][d]
 }
 
