f87e33c5f78e0985360d9ce3326969266dbb879f
gaoruixian <gaoruixian@chuxing365.com>
2023-05-19 17:40:29 +0800

    1079 dynamicprogramming,depth first search

---
+++ leetcode/1079numTilePossibilities_test.go
@@ -0,0 +1,58 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func numTilePossibilities(tiles string) int {
+	size := len(tiles)
+	countMap := make(map[string]int)
+	for _, v := range tiles {
+		countMap[string(v)]++
+	}
+	//fmt.Println(countMap)
+	// A，A，B
+	for i := 1; i <= size; i++ {
+
+	}
+	return dfs(len(tiles), countMap) - 1
+
+}
+func dfs(i int, count map[string]int) int {
+	fmt.Println(i, ",", count)
+	if i == 0 {
+		return 1
+	}
+	//fmt.Println(i, ",", count)
+	res := 1
+	c := 0
+	for t := range count {
+		c++
+		if count[t] > 0 {
+			count[t]--
+			fmt.Println(t, "before", count, ",", c)
+			res = res + dfs(i-1, count)
+			count[t]++
+			fmt.Println(t, "after:", count)
+		}
+	}
+	return res
+}
+func TestNumTitlePossibilities(t *testing.T) {
+	tiles := "AAB"
+	//tiles = "AAABBC"
+
+	fmt.Println(numTilePossibilities(tiles))
+}
+
+func Test1(t *testing.T) {
+	a := "A"
+	tiles := "AAB"
+	tiles = "AAABBC"
+	for _, v := range tiles {
+		fmt.Println(string(v))
+
+	}
+	fmt.Println(a)
+}
